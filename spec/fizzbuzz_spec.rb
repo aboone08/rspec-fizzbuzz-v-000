fcc1d9704cd6cd81fd0d6ad15bcfefee67ec492b
Andranique Boone <andranique.boone@gmail.com>
2018-09-13 22:14:14 +0000

    automatically backed up by learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,12 +3,12 @@ require_relative './spec_helper.rb'
 require_relative '../fizzbuzz.rb'
 describe "fizzbuzz" do
     def fizzbuzz(num)
-    if num % 3 == 0 
-       "Fizz"
-    elsif num % 5 == 0 
-       "Buzz"
-    elsif num % 3 == 0 && num % 5 == 0 
+    if  num % 3 == 0 && num % 5 == 0 
        "FizzBuzz"
+    elsif num % 3 == 0 
+       "Fizz"
+    elsif num % 5 == 0
+       "Buzz"
     else 
       puts "nil"
     end
