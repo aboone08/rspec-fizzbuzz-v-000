d5c7bff1dece365abb4d54c6c92d73002e81d00a
Andranique Boone <andranique.boone@gmail.com>
2018-09-13 21:49:10 +0000

    automatically backed up by learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,7 +2,7 @@ require_relative './spec_helper.rb'
 # We are missing a 'require_relative' here. Check README.md for instructions and an explanation! 
 require_relative '../fizzbuzz.rb'
 describe "fizzbuzz" do
-  def fizzbuzz()
+  def fizzbuzz(num)
     if num % 3 == 0 
       puts "Fizz"
     elsif num % 5 == 0 
