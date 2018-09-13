aa93645688e511bd549be8c8ec1bd28e75c9108b
Andranique Boone <andranique.boone@gmail.com>
2018-09-13 21:53:15 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz
-   def fizzbuzz(num)
     if num % 3 == 0 
       puts "Fizz"
     elsif num % 5 == 0 
@@ -11,5 +10,4 @@ def fizzbuzz
     else 
       puts "nil"
     end
-  end
 end
