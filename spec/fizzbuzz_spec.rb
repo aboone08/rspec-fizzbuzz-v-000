113d5f69095878f5e5ff9ce79db600b12b93a6e9
Andranique Boone <andranique.boone@gmail.com>
2018-09-13 21:54:49 +0000

    automatically backed up by learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,6 +2,7 @@ require_relative './spec_helper.rb'
 # We are missing a 'require_relative' here. Check README.md for instructions and an explanation! 
 require_relative '../fizzbuzz.rb'
 describe "fizzbuzz" do
+  def fizzbuzz(num)
     if num % 3 == 0 
       puts "Fizz"
     elsif num % 5 == 0 
@@ -10,7 +11,9 @@ describe "fizzbuzz" do
       puts "FizzBuzz"
     else 
       puts "nil"
+      
     end
+  end
   it 'returns "Fizz" when the number is divisible by 3' do
     fizz_3 = fizzbuzz(3)
 
