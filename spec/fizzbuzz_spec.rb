5e4420774d1fe2f849fc6efcd9e47acb7494017c
Andranique Boone <andranique.boone@gmail.com>
2018-09-13 21:38:27 +0000

    automatically backed up by learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,11 +4,11 @@ require_relative '../fizzbuzz.rb'
 describe "fizzbuzz" do
   def fizzbuzz(int)
     if int % 3 == 0 
-      "Fizz"
-    end
+      puts "Fizz"
     else int % 5 == 0 
-      "Buzz"
-    end 
+      puts "Buzz"
+    elsif int % 3 || % 5 == 0 
+      puts "FizzBuzz"
   end
   it 'returns "Fizz" when the number is divisible by 3' do
     fizz_3 = fizzbuzz(3)
